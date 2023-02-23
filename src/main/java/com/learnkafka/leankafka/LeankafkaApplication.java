ab900a5ab32b859e908c8738b7c81fe13f6e8d3b
sondn <dangngocsoncntt@gmail.com>
2023-02-23 15:06:57 +0700

    cap nhat moi du an

---
+++ src/main/java/com/learnkafka/leankafka/LeankafkaApplication.java
@@ -0,0 +1,13 @@
+package com.learnkafka.leankafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeankafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeankafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnkafka/leankafka/LeankafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnkafka.leankafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeankafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
